7b6a6c564071d6341a3c0ef8800d4007d363a346
CondoriM <condoria.miguel@gmail.com>
2022-03-29 16:38:55 +0200

    rendom number code

--- assets/main.js
+++ assets/main.js
@@ -15,3 +15,21 @@ function loginUser(){
         }
     }
 }
+
+
+//ramdom number
+function randomM(){
+    let num1 = Math.floor(Math.random() * 6) + 1;
+    console.log(num1);
+
+    let num2 = Math.floor(Math.random() * 6) + 1;
+    console.log(num2);
+
+    if (num1 > num2){
+        document.getElementById('vincitore').innerHTML = "Ha vinto il giocatore Numero1"
+    } else if (num2 > num1){
+        document.getElementById('vincitore').innerHTML = "Ha vinto il giocatore Numero2"
+    } else {
+        document.getElementById('vincitore').innerHTML = "Pareggio"
+    }
+}
